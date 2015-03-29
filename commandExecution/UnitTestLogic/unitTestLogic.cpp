127b4554e17a729b173834e671fd8a9c9a1391de
Jason Yang Hong Jie <jhyapril@gmail.com>
2015-03-29 21:36:55 +0800

    unittest logic cpp

---
+++ commandExecution/UnitTestLogic/unitTestLogic.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "commandExecution.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLogic
+{		
+	TEST_CLASS(UnitTestPerformAdd)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+			CommandExecution test;
+			string expected = "Successfully added 'on July 10 from 1400 to 1600 play basketball' to the schedule\r\n";
+			string actual = test.readCommand("add July 10 -s 1400 -e 1600 play basketball");
+			Assert::AreEqual(expected, actual);
+		}
+	};
+
+	TEST_CLASS(UnitTestPerformDelete)
+	{
+	public:
+		
+		TEST_METHOD(TestPerformDelete1) {
+			//This is a boundary case for the 'value < 1' partition 
+			CommandExecution test;
+			string expected = "Invalid index!\r\n";
+			string actual = test.readCommand("delete 0");
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestPerformDelete2) {
+			//this is a boundary case for the 'value > size' partition
+			CommandExecution test;
+			string expected = "Invalid index!\r\n";
+			string actual = test.readCommand("delete 1");
+			Assert::AreEqual(expected, actual);
+		}
+
+	};
+
+}
